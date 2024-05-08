05821fa55fed58f19c63827586d66d620a0a1d33
PyFon <theravent19@gmail.com>
2024-05-08 16:10:38 +0200

    a

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from excel_for_app.marged_and_imported_data import data_for_app
-
+from gui.gui import bank_gui
+import tkinter as tk
 # Define the input file
 input_file = "lista_operacji_240401_240418_202404181243325270.csv"
 
 # Call the data_for_app function
-p = data_for_app(input_file)
+if __name__ == "__main__":
+    root = tk.Tk()
+    bank_gui(root)
+    root.mainloop()
